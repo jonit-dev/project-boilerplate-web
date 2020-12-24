e85457dd6c1ac7947e2c7c45f6263b1f7bf5265b
Joao Paulo Furtado <joaopaulofurtado@live.com>
2020-12-23 23:41:45 -0800

    fixed babel issue

---
+++ styles/home.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: green;
+`;
+
+const H1 = styled.h1`
+  color: red;
+`;
+
+export const HomeStyled = {
+  Container,
+  H1
+};
