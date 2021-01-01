9efb3105d5a397072c76cfd2c3ca01d3661ac987
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-01-01 15:24:04 -0800

    removed useless react import from _document

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
-import { useEffect } from "react";
-
 import { HomeStyled } from "../styles/home.styles";
 
 export default function Home() {
-  useEffect(() => {});
-
   return (
     <HomeStyled.Container>
       <HomeStyled.H1>Hello World!</HomeStyled.H1>
