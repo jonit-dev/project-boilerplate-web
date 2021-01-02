38f4dc35464edca92dafa251cc049217242a6b38
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-01-02 14:17:15 -0800

    better getStaticProps example

--- pages/static-props.tsx
+++ pages/static-props.tsx
@@ -1,19 +1,37 @@
+import axios from "axios";
 import { GetStaticProps } from "next";
 
-export const getStaticProps: GetStaticProps = async (context) =>
-  // This will run on server-side, to generate this screen as a static file, useful for performance reasons (pages that are visited thousands of times per day and could be served statically to save resources!)
+import { IUser } from "../types/user.types";
 
-  ({
+/*#############################################################|
+|  >>> STATICPROPS EXAMPLE
+*##############################################################*/
+
+// getStaticProps will run on server-side, to generate this screen as a static file, useful for performance reasons (pages that are visited thousands of times per day and could be served statically to save resources!)
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+
+  const users = res.data as IUser[];
+
+  return {
     revalidate: 10, // it will try to update the static file every 10 seconds only, to increase performance!
     props: {
-      myFavNum: 5,
+      users,
     },
-  });
+  };
+};
 
-export default function StaticPropsExample({ myFavNum }) {
-  return (
-    <div>
-      <p>This is a static page. Number: {myFavNum}</p>
-    </div>
-  );
+export default function StaticPropsExample({ users }) {
+  const onRenderUsers = (users: IUser[]) => {
+    return users.map((user) => (
+      <ul key={user.id}>
+        <li>{user.name}</li>
+        <li>{user.email}</li>
+        <li>{user.phone}</li>
+      </ul>
+    ));
+  };
+
+  return <div>{onRenderUsers(users)}</div>;
 }
