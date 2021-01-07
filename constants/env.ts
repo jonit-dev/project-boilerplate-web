edff43b770a4da78c02c22a59d3bf5c1df37abc8
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-01-06 21:30:14 -0800

    constants file

---
+++ constants/env.ts
@@ -0,0 +1,11 @@
+
+
+export const app = {
+  general: {
+    env: process.env.NEXT_PUBLIC_ENV,
+    name: process.env.NEXT_PUBLIC_APP_NAME,
+    language: process.env.NEXT_PUBLIC_APP_LANGUAGE,
+    apiUrl: process.env.NEXT_PUBLIC_API_URL,
+    appUrl: process.env.NEXT_PUBLIC_APP_URL
+  },
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
-
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
