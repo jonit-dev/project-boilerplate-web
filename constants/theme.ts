75f2e007995bb1f5731b32130a2e8a64df512338
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-01-10 00:03:26 -0800

    theme.ts

---
+++ constants/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    primary: "#0D85F6",
+    secondary: "#02CBB1",
+    dark: "#1E2128",
+    mediumGray: "#8A94A7",
+    darkGray: "#434853",
+  },
+  typography: {
+    baseFont: "IBM Plex Sans",
+  },
+};
