fda89746c8574b33e7bdfb265a392059b99d4c34
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-01-10 21:25:57 -0800

    refactored scss custom theme

--- styles/components/card.styles.ts
+++ styles/components/card.styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Base = styled.div`
   background-color: white;
   border-radius: 5px;
-  box-shadow: 3px 2px 5px -2px rgba(0, 0, 0, 0.51);
+  box-shadow: 3px 2px 5px -2px rgba(0, 0, 0, 0.2);
   padding: 2rem 2rem;
 `;
 
